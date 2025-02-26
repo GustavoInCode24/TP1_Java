9978e6c913e6d2e251a577d3ffd60c59f578ea52
GustavoInCode24 <gugaprogramador24@gmail.com>
2025-02-26 14:47:24 -0300

    Update tranposiçcaoMatriz.java

--- Ex_Matriz/src/tranposiçãoMatriz/tranposiçcaoMatriz.java
+++ Ex_Matriz/src/tranposiçãoMatriz/tranposiçcaoMatriz.java
@@ -3,7 +3,41 @@ package tranposiçãoMatriz;
 public class tranposiçcaoMatriz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Quantas linhas tem a sua matriz?: ");
+		int linha = scan.nextInt();
+		System.out.print("Quantas colunas tem a sua matriz?: ");
+		int coluna = scan.nextInt();
+		
+		int[][] matriz = new int[linha][coluna];
+		
+		for(int i = 0; i < linha; i++) {
+			for(int j = 0; j < coluna; j++) {
+				System.out.print("Elemento [" + i + "][" + j + "]: ");
+				matriz[i][j] = scan.nextInt();
+			}
+		}
+
+		//Criando a matriz transposta
+		int[][] matrizTransposta = new int[coluna][linha];
+		for(int i = 0; i < linha; i++) {
+			for(int j = 0; j < coluna; j++) {
+				matrizTransposta[j][i] = matriz[i][j];
+			}
+		}
+		System.out.println(" ");
+		System.out.println("Matriz transposta:");
+        for (int i = 0; i < coluna; i++) {
+            for (int j = 0; j < linha; j++) {
+                System.out.print(matrizTransposta[i][j] + " "); 
+            }
+            System.out.println(); 
+        }
+        scan.close();
+	}
+
+}
 
 	}
 
